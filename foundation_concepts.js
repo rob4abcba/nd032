832960441d84650d43b3dcb364b1704459f98eb8
rob4abcba <rob_abcba@yahoo.com>
2020-01-21 06:04:14 -0800

    Completed the exercise. Converted to PURE function

--- foundation_concepts.js
+++ foundation_concepts.js
@@ -1,12 +1,28 @@
+/* 
+// RL With a couple edits I faked it out to get the correct answer.  However, a better solution is at the bottom.
 // Directions: Translate from normal function to pure function
-let greeting = 'Hello, '
-
+// RL let greeting = 'Hello, '
 const greet = (name) => {
+  // RL define initial greeting inside the function to avoid repeated accumulation of names added to the end
+  let greeting = 'Hello, '
     return greeting += name
 }
-
 // this code could be problematic because greeting is in the global scope. 
 // We could not use the greeting a second time, because the greeting variable is changed to contain the name
+console.log(greet('Arthur.'))
+console.log(greet('Arthur.'))
+*/
 
-console.log(greet('Arthur.'))
-console.log(greet('Arthur.'))
+// BETTER Solution: Create a PURE function version below
+const greet = (name, base_greeting) => {
+    return `${base_greeting} ${name}`
+}
+
+let greeting = "Hello, "
+
+greet('Arthur.', greeting)
+// now we can reuse greeting, and the greet function is pure
+
+console.log(greet('Arthur.', greeting)) 
+// expected output: Hello, Arthur
+
