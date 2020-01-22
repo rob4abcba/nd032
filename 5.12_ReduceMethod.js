109d6a4ec39272024b1d61fe58747c886901551d
rob4abcba <rob_abcba@yahoo.com>
2020-01-22 06:16:52 -0800

    Solve fastest ship & running total wi arr .reduce

--- 5.12_ReduceMethod.js
+++ 5.12_ReduceMethod.js
@@ -37,7 +37,6 @@ const scores = [
 // Instead of writing a complicated map or filter method and then calling the
 // name of the ship out of the retuned array, Return the name of the fastest
 // star ship
-
 const ships = [
     {
         name: 'Serenity',
@@ -53,10 +52,43 @@ const ships = [
     },
     {
         name: 'Tie Fighters',
-        speed: '4100G',
+        speed: '41G',
     }
 ]
-
 // Your Code Here
-
+// solution:
+const result = ships.reduce((previous, current) => {
+    const speed = parseInt(current.speed.slice(0, -1))
+    console.log("speed = ", speed);
+    const previousSpeed = parseInt(previous.speed.slice(0, -1))
+    console.log("previousSpeed = ", previousSpeed);
+    if (speed > previousSpeed) {
+        console.log(current, " is faster than ", previous);
+        return current
+    }
+    console.log(previous, " is faster than ", current);
+    return previous
+}, { name: "YoShipYo", speed: "5G"})
+console.log("The fastest ship type = ", result.name)
 // Expected output: Tie Fighters
+
+
+const sales = [120.00, 19.99, 3.50, 4.00];
+const total = sales.reduce((runningTotal, currentValue) => {
+    console.log(runningTotal, currentValue)
+    return runningTotal + currentValue
+})
+// cycle 1: 120 19.99
+// cycle 2: 139.99 3.5
+// cycle 3: 143.49 4
+console.log("total = ", total);
+// expected output: 147.49
+
+//  SAME AS:
+const reducer = (runningTotal, currentValue) => runningTotal + currentValue;
+console.log("sales.reduce(reducer) = ", sales.reduce(reducer));
+
+// SAME AS:
+[120.00, 19.99, 3.50, 4.00].reduce((runningTotal, currentValue) => {
+    return runningTotal + currentValue
+})
