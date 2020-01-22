984cfb8f178560bc5bfb2dd9a1ad079559f16ea7
rob4abcba <rob_abcba@yahoo.com>
2020-01-22 01:06:18 -0800

    Add 5.9 Fiilter Method Exercise

---
+++ 5.9_FiilterMethod.js
@@ -0,0 +1,61 @@
+// 1. Find all the words with more than 7 characters
+const words = ['tardis', 'grok', 'frak', 'blaster', 'klingon', 'shepherd']
+
+// Your Code
+
+// expected output: Array ['shepherd']
+
+// ----------------------------------------------------------
+// 2. Find all even values
+const words = [12, 13, 14, 15, 16, 17]
+
+// Your Code
+
+// expected output: Array [12, 14, 16]
+
+// ----------------------------------------------------------
+// REAL LIFE EXAMPLE
+// We often use filter to quickly pull all the items that share a status or other characteristic. For instance, create a list of all the active bounty hunters from the array below:
+
+const hunters = [
+    {
+        name: 'Greedo',
+        universe: 'Star Wars',
+        status: 'active',
+    },
+    {
+        name: 'Boba Fett',
+        universe: 'Star Wars',
+        status: 'inactive',
+    },
+    {
+        name: 'Asajj Ventress',
+        universe: 'Star Wars',
+        status: 'unknown',
+    },
+    {
+        name: 'Zam Wesell',
+        universe: 'Star Wars',
+        status: 'inactive',
+    },
+    {
+        name: 'Jango Fett',
+        universe: 'Star Wars',
+        status: 'active',
+    },
+]
+
+// Your code
+
+// expected output: Array [
+//     {
+//         name: 'Greedo',
+//         universe: 'Star Wars',
+//         status: 'active',
+//     },
+//     {
+//         name: 'Jango Fett',
+//         universe: 'Star Wars',
+//         status: 'active',
+//     },
+// ]
