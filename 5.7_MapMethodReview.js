3dd12fe2ba7a7b4f56cc8e2c669a547efbb01b15
rob4abcba <rob_abcba@yahoo.com>
2020-01-22 01:03:49 -0800

    Add 2 Advanced Exercises @End. parseInt. Reformat.

--- 5.7_MapMethodReview.js
+++ 5.7_MapMethodReview.js
@@ -57,3 +57,40 @@ const sci_fi_shows2 = [1, 2, 3, 4]
 const result23 = sci_fi_shows1.map((intergerYo, index) => sci_fi_shows2)
 console.log(result23)
 // expected output: Array [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+
+
+// ----------------------------------------------------------
+// ADVANCED EXERCISES
+// ----------------------------------------------------------
+// Directions: 
+
+// ["1", "2", "3"].map(parseInt)
+console.log('["1", "2", "3"].map(parseInt) = ', ["1", "2", "3"].map(parseInt));
+// const parseIntArray = ["2", "1", "3"].map(parseInt);
+const base = 10;
+// const parseIntArray = ["2", "1", "3"].map(parseInt(item, base));
+const parseIntArray = ["2", "1", "3"].map((item) => parseInt(item, base));
+console.log('parseIntArray = ', parseIntArray );
+
+// ----------------------------------------------------------
+// REAL LIFE EXERCISES
+// ----------------------------------------------------------
+
+// One common thing we use map for in real life is to reformat objects to have a shape that is better for our purposes. 
+// For instance, we only care about the name and first three characters of the id for the races below. 
+// Use map to grab those values and create a new array with them.
+
+var index = [
+    {key: 1, sector: 10, t_score: 18, id: '1236n7e8', value: 'Klingon'},
+    {key: 4, sector: 145, t_score: 12, id: '293847hs8', value: 'Minbari'},
+    {key: 8, sector: 214, t_score: 5, id: '283hy8347', value: 'Cylon'},
+    {key: 3, sector: 8346, t_score: 10, id: 'n9837ks857', value: 'Jawa'},
+]
+console.log(index.map( (item) => `${item.value} ${item.id.substring(0,3)}`))
+
+// Another solution
+function getNameId3Char(item) {
+    var NameId3Char = [item.value,item.id.substring(0,3)].join(" ");
+    return NameId3Char;
+  }
+  console.log(index.map(getNameId3Char));
