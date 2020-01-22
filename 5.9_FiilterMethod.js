2a836d3ed9763897fe02bfdb680bca959caf0e7b
rob4abcba <rob_abcba@yahoo.com>
2020-01-22 01:35:55 -0800

    Add 4th exercise. Solve all 4 exercises use Filter

--- 5.9_FiilterMethod.js
+++ 5.9_FiilterMethod.js
@@ -1,21 +1,23 @@
 // 1. Find all the words with more than 7 characters
-const words = ['tardis', 'grok', 'frak', 'blaster', 'klingon', 'shepherd']
-
+const words1 = ['tardis', 'grok', 'frak', 'blaster', 'klingon', 'shepherd']
 // Your Code
-
+const result1 = words1.filter((item) => { return item.length > 7 } );
+console.log("result1 = ", result1)
+console.log("words1.filter((item) => { return item.length > 7 } ) = ", result1)
 // expected output: Array ['shepherd']
 
 // ----------------------------------------------------------
 // 2. Find all even values
-const words = [12, 13, 14, 15, 16, 17]
-
+const words2 = [12, 13, 14, 15, 16, 17]
 // Your Code
-
+const result2 = words2.filter((item) => { return item%2 == 0 } );
+console.log("result2 = ", result2)
 // expected output: Array [12, 14, 16]
 
 // ----------------------------------------------------------
 // REAL LIFE EXAMPLE
-// We often use filter to quickly pull all the items that share a status or other characteristic. For instance, create a list of all the active bounty hunters from the array below:
+// We often use filter to quickly pull all the items that share a status or other characteristic. 
+// For instance, create a list of all the active bounty hunters from the array below:
 
 const hunters = [
     {
@@ -46,7 +48,8 @@ const hunters = [
 ]
 
 // Your code
-
+const result3 = hunters.filter((item) => { return item.status == 'active' } );
+console.log("result3 = ", result3)
 // expected output: Array [
 //     {
 //         name: 'Greedo',
@@ -59,3 +62,10 @@ const hunters = [
 //         status: 'active',
 //     },
 // ]
+
+// Video examples
+// ----------------------------------------------------------
+const values = ['true', true, 'yes', 'no', 1, 0, 'false', false];
+const result4 = values.filter(v => v == true);
+console.log("result4 = ", result4);
+// expected output: Array [true, 1]
